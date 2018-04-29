dcc989c04ae301e7ab03f4ee79a0b7dae7a5a348
Igor Hitzschky <igor.lema@ingressorapido.com.br>
2018-04-29 15:21:48 -0300

    add all queries

--- src/main/kotlin/com/mc920/xcommerce/dao/ProductDaoPostgres.kt
+++ src/main/kotlin/com/mc920/xcommerce/dao/ProductDaoPostgres.kt
@@ -1,22 +1,35 @@
 package com.mc920.xcommerce.dao
 
 import org.springframework.jdbc.core.JdbcTemplate
+import java.sql.PreparedStatement
 
-class ProductDaoPostgres(val jdbcTemplate: JdbcTemplate) : ProductDao {
+class Queries {
+    companion object {
+        const val INSERT_EXTERNAL_ID = """INSERT INTO xcommerce.product_relation(external_id) VALUES (?) RETURNING id"""
+        const val FIND_BY_ID = """SELECT external_id FROM xcommerce.product_relation"""
+        const val FIND_BY_EXTERNAL_ID = """SELECT id FROM xcommerce.product_relation"""
+    }
+}
+
+class ProductDaoPostgres(private val jdbcTemplate: JdbcTemplate) : ProductDao {
 
     override fun findById(id: Long): Long? {
-        return jdbcTemplate.query("SELECT external_id FROM product_relation", { rs, _ ->
+        return jdbcTemplate.query(Queries.FIND_BY_ID, { rs, _ ->
             rs.getLong("external_id")
         }).firstOrNull()
 
     }
 
     override fun insertExternalId(externalId: Long): Long? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return jdbcTemplate.query(Queries.INSERT_EXTERNAL_ID,
+            { ps: PreparedStatement ->
+                ps.setLong(1, externalId) },
+            { rs, _ ->
+                rs.getLong("id") }).firstOrNull()
     }
 
     override fun findByExternalId(externalId: Long): Long? {
-        return jdbcTemplate.query("SELECT id FROM product_relation", { rs, _ ->
+        return jdbcTemplate.query(Queries.FIND_BY_EXTERNAL_ID, { rs, _ ->
             rs.getLong("id")
         }).firstOrNull()
     }
