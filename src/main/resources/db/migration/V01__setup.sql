df7a9839571dc71b549aaf03a2a7d81e4c27d4d7
Igor Hitzschky <igor.lema@ingressorapido.com.br>
2018-04-29 19:13:38 -0300

    more config

--- src/main/resources/db/migration/V01__setup.sql
+++ src/main/resources/db/migration/V01__setup.sql
@@ -6,21 +6,21 @@ BEGIN
       FROM   pg_catalog.pg_user
       WHERE  usename = 'xcom-user') THEN
 
-      CREATE USER 'xcom-user' WITH password 'xcom-pass';
+      CREATE USER xcom-user WITH password 'xcom-pass';
    END IF;
 END
 $body$;
 
 CREATE SCHEMA IF NOT EXISTS xcommerce;
 
-GRANT USAGE ON SCHEMA xcommerce TO 'xcom-user';
+GRANT USAGE ON SCHEMA xcommerce TO xcom-user;
 
 ALTER DEFAULT PRIVILEGES
    IN SCHEMA xcommerce
 GRANT SELECT, UPDATE, INSERT, DELETE ON TABLES
-   TO 'xcom-user';
+   TO xcom-user;
 
 ALTER DEFAULT PRIVILEGES
    IN SCHEMA xcommerce
 GRANT USAGE ON SEQUENCES
-   TO 'xcom-user';
+   TO xcom-user;
