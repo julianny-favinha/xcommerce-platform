9b6c57555c8ffbe8e7788576be28cf15b5e1f7a4
Igor Hitzschky <igor.lema@ingressorapido.com.br>
2018-04-29 19:11:32 -0300

    more properties

--- src/main/resources/db/migration/V01__setup.sql
+++ src/main/resources/db/migration/V01__setup.sql
@@ -1,13 +1,26 @@
+DO
+$body$
+BEGIN
+   IF NOT EXISTS (
+      SELECT *
+      FROM   pg_catalog.pg_user
+      WHERE  usename = 'xcom-user') THEN
+
+      CREATE USER 'xcom-user' WITH password 'xcom-pass';
+   END IF;
+END
+$body$;
+
 CREATE SCHEMA IF NOT EXISTS xcommerce;
 
-GRANT USAGE ON SCHEMA xcommerce TO igorlema;
+GRANT USAGE ON SCHEMA xcommerce TO 'xcom-user';
 
 ALTER DEFAULT PRIVILEGES
    IN SCHEMA xcommerce
 GRANT SELECT, UPDATE, INSERT, DELETE ON TABLES
-   TO igorlema;
+   TO 'xcom-user';
 
 ALTER DEFAULT PRIVILEGES
    IN SCHEMA xcommerce
 GRANT USAGE ON SEQUENCES
-   TO igorlema;
+   TO 'xcom-user';
