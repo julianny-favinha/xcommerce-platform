ecfc02d8e5c4acb95bbea248c79c9793d5f977fd
Igor Hitzschky <igor.lema@ingressorapido.com.br>
2018-05-11 21:13:24 -0300

    add signup

--- src/main/kotlin/com/mc851/xcommerce/controller/UserController.kt
+++ src/main/kotlin/com/mc851/xcommerce/controller/UserController.kt
@@ -4,6 +4,7 @@ import com.mc851.xcommerce.model.SignUp
 import com.mc851.xcommerce.model.User
 import com.mc851.xcommerce.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,8 +23,9 @@ class UserController {
     }
 
     @PostMapping("/signUp")
-    fun signUp(@RequestBody signUp: SignUp) {
-        userService.signUp(signUp)
+    fun signUp(@RequestBody signUp: SignUp): ResponseEntity<User> {
+        val response = userService.signUp(signUp)
+        return handleResponse(response)
     }
 
     // addAddress
