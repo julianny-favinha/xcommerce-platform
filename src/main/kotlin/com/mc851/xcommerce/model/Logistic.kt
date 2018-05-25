bc923d23c7c45f8ee2c202dbdada49eaf87ae930
Yuri Delgado <ydelgado@localhost.localdomain>
2018-05-25 17:05:01 -0300

    correction: remove cepFrom from props in Shipment objects

--- src/main/kotlin/com/mc851/xcommerce/model/Logistic.kt
+++ src/main/kotlin/com/mc851/xcommerce/model/Logistic.kt
@@ -2,9 +2,9 @@ package com.mc851.xcommerce.model
 
 data class ShipmentOut(val prices: Map<String, Int>)
 
-data class ShipmentIn(val products: List<Product>, val cepFrom: String, val cepDst: String)
+data class ShipmentIn(val products: List<Product>, val cepDst: String)
 
-data class ShipmentInIndividual(val product: Product, val cepFrom: String, val cepDst: String)
+data class ShipmentInIndividual(val product: Product, val cepDst: String)
 
 data class OrderIn(val product: Product, val cepDst: String)
 
