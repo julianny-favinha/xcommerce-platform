5d1630a02e9010674ac126d07dc4cf92803f5723
Igor Hitzschky <igor.lema@ifood.com.br>
2018-06-10 20:08:44 -0300

    add some logs

--- src/main/kotlin/com/mc851/xcommerce/clients/payment/PaymentClientOkHttp.kt
+++ src/main/kotlin/com/mc851/xcommerce/clients/payment/PaymentClientOkHttp.kt
@@ -8,6 +8,7 @@ import com.mc851.xcommerce.clients.payment.api.PaymentInCreditCard
 import com.mc851.xcommerce.clients.payment.api.PaymentOutBoleto
 import com.mc851.xcommerce.clients.payment.api.PaymentOutCreditCard
 import com.mc851.xcommerce.clients.payment.api.StatusBoleto
+import mu.KotlinLogging
 import okhttp3.HttpUrl
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
@@ -18,12 +19,16 @@ class PaymentClientOkHttp : PaymentClient {
 
     private val okHttpClient = OkHttpClient()
     private val objectMapper = jacksonObjectMapper()
+    private val log = KotlinLogging.logger {}
 
     override fun creditCardPayment(paymentIn: PaymentInCreditCard): PaymentOutCreditCard? {
         val httpUrl = HttpUrl.parse("https://payment-server-mc851.herokuapp.com/payments/creditCard")!!.newBuilder()
 
+        val body = objectMapper.writeValueAsString(paymentIn)
+        log.info { "Credit Card Payment: $body to https://payment-server-mc851.herokuapp.com/payments/creditCard" }
+
         val jsonType = MediaType.parse("application/json")
-        val requestBody = RequestBody.create(jsonType, objectMapper.writeValueAsString(paymentIn))
+        val requestBody = RequestBody.create(jsonType, body)
 
         val request = Request.Builder().post(requestBody).url(httpUrl.build().toString()).build()
         val response = okHttpClient.newCall(request).execute()
@@ -32,23 +37,31 @@ class PaymentClientOkHttp : PaymentClient {
             return null
         }
 
-        return objectMapper.readValue<PaymentOutCreditCard>(response.body()!!.byteStream())
+        val responseBody = response.body()
+        log.info { responseBody }
+        return objectMapper.readValue(responseBody!!.byteStream())
     }
 
     override fun boletoPayment(paymentIn: PaymentInBoleto): PaymentOutBoleto? {
         val httpUrl = HttpUrl.parse("https://payment-server-mc851.herokuapp.com/payments/bankTicket")!!.newBuilder()
 
+        val body = objectMapper.writeValueAsString(paymentIn)
+        log.info { "Boleto Payment: $body to https://payment-server-mc851.herokuapp.com/payments/bankTicket" }
+
         val jsonType = MediaType.parse("application/json")
-        val requestBody = RequestBody.create(jsonType, objectMapper.writeValueAsString(paymentIn))
+        val requestBody = RequestBody.create(jsonType, body)
 
         val request = Request.Builder().post(requestBody).url(httpUrl.build().toString()).build()
         val response = okHttpClient.newCall(request).execute()
+
 
         if (!response.isSuccessful) {
             return null
         }
 
-        return objectMapper.readValue<PaymentOutBoleto>(response.body()!!.byteStream())
+        val responseBody = response.body()
+        log.info { responseBody }
+        return objectMapper.readValue(responseBody!!.byteStream())
     }
 
     override fun statusBoletoPayment(code: String): StatusBoleto? {
