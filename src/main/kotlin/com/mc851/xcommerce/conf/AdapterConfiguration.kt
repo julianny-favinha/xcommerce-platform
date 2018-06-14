c099bb0e532237e0c1a99a903120a8aef806daf9
Igor Hitzschky <igor.lema@ifood.com.br>
2018-06-13 23:52:05 -0300

    mini-fix

--- src/main/kotlin/com/mc851/xcommerce/conf/AdapterConfiguration.kt
+++ src/main/kotlin/com/mc851/xcommerce/conf/AdapterConfiguration.kt
@@ -22,7 +22,7 @@ class AdapterConfiguration : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(requestFilter)
-        registry.addInterceptor(tokenManager).addPathPatterns("/cart/checkout/**", "/user", "/user/update", "/order", "/sac/**")
+        registry.addInterceptor(tokenManager).addPathPatterns("/cart/checkout/**", "/user", "/user/update", "/order/**", "/order", "/sac/**")
         super.addInterceptors(registry)
     }
 }
